dec291b8d5ee7d46fd75c7e838cff44e554d5cf5
Diego Cirilo <dvcirilo@gmail.com>
2022-10-28 16:06:36 -0300

    zeep

--- src/exemplo04.py
+++ src/exemplo04.py
@@ -3,29 +3,6 @@ import zeep
 # define a URL do WSDL
 wsdl_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL"
 
-# define a URL do método
-method_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryIntPhoneCode"
-
-# define a URL do serviço
-service_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso"
-
-# cria o header
-header = zeep.xsd.Element(
-	"Header",
-	zeep.xsd.ComplexType(
-		[
-			zeep.xsd.Element(
-				"{http://www.w3.org/2005/08/addressing}Action", zeep.xsd.String()
-			),
-			zeep.xsd.Element(
-				"{http://www.w3.org/2005/08/addressing}To", zeep.xsd.String()
-			),
-		]
-	),
-)
-# define o valor do header a partir do objeto header
-header_value = header(Action=method_url, To=service_url)
-
 # inicializa o cliente zeep
 client = zeep.Client(wsdl=wsdl_url)
 
@@ -34,8 +11,7 @@ country_code = "BR"
 
 # faz a chamada do serviço
 result = client.service.CountryIntPhoneCode(
-	sCountryISOCode=country_code,
-	_soapheaders=[header_value]
+	sCountryISOCode=country_code
 )
 # imprime o resultado
 print(f"O código de telefone do {country_code} é {result}")
@@ -45,8 +21,7 @@ country_code = "US"
 
 # faz a chamada do serviço
 result = client.service.CountryIntPhoneCode(
-	sCountryISOCode=country_code,
-	_soapheaders=[header_value]
+	sCountryISOCode=country_code
 )
 
 # imprime o resultado
